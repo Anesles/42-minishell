ac905588a56c6f1001be4f8c328a832816023cf3
Anesles <bbmp2002@gmail.com>
2023-06-13 20:05:55 +0100

    Fixed parser (kinda)

--- minishell/srcs/bash/parser.c
+++ minishell/srcs/bash/parser.c
@@ -6,7 +6,7 @@
 /*   By: brumarti <brumarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 15:18:16 by brumarti          #+#    #+#             */
-/*   Updated: 2023/06/07 18:51:28 by brumarti         ###   ########.fr       */
+/*   Updated: 2023/06/13 20:02:38 by brumarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,11 +28,24 @@ int	check_commands(char *cmd)
 		return (EXIT_SUCCESS);
 }
 
-void	parser_aux(t_cmds *cmds, t_mshell *mshell, int pid)
+void	wait_fork(int *status, int pid, t_mshell *mshell, char *cmd)
+{
+	if (!ft_strncmp(cmd, "grep", 4))
+	{
+		close(mshell->fd[0]);
+		close(mshell->fd[1]);
+	}
+	waitpid(pid, status, 0);
+}
+
+void	parser_aux(t_cmds *cmds, t_mshell *mshell)
 {
 	int	i;
+	int	pid;
+	int	status;
 
 	i = -1;
+	status = 0;
 	while (++i < mshell->n_cmds)
 	{
 		mshell->current_cmd++;
@@ -45,46 +58,21 @@ void	parser_aux(t_cmds *cmds, t_mshell *mshell, int pid)
 			{
 				handle_pipe(mshell);
 				cmds[i].built(&cmds[i], mshell);
-				exit(0);
 			}
+			else
+				wait_fork(&status, pid, mshell, cmds[i].words[0]);
+			g_exit_status = WEXITSTATUS(status);
 		}
 	}
-}
-
-void	parser_aux_2(t_cmds *cmds, t_mshell *mshell, int pid, int status)
-{
-	if (is_builtins(cmds->words[0]))
-		g_exit_status = cmds->built(cmds, mshell);
-	else
-	{
-		pid = fork();
-		if (pid == 0)
-		{
-			cmds[0].built(&cmds[0], mshell);
-			exit(0);
-		}
-		else
-			waitpid(pid, &status, 0);
-		g_exit_status = WEXITSTATUS(status);
-	}
-	mshell->current_cmd++;
 }
 
 void	parser(t_cmds *cmds, t_mshell *mshell)
 {
-	pid_t	pid;
-	int		status;
-
-	status = 0;
-	pid = 0;
 	if (mshell->n_cmds > 1)
-	{
-		parser_aux(cmds, mshell, pid);
-		close(mshell->fd[0]);
-		close(mshell->fd[1]);
-		waitpid(pid, &status, 0);
-		g_exit_status = WEXITSTATUS(status);
-	}
+		parser_aux(cmds, mshell);
 	else
-		parser_aux_2(cmds, mshell, pid, status);
+	{
+		mshell->current_cmd++;
+		cmds[0].built(&cmds[0], mshell);
+	}
 }
